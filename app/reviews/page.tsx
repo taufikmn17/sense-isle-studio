3aa6b8322d6d4500e9f36c252c007512a6026d90
Taufik Mustafa <taufikmustafa20@gmail.com>
2026-09-14 16:00:29 +0700

    ulasan

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -67,7 +67,7 @@ export default function ReviewsPage() {
       <section className="relative w-full min-h-[85vh] py-16 px-4 md:px-12 flex items-center justify-center overflow-hidden">
         {/* Background Image */}
         <Image
-          src="/images/reviewss.png"
+          src="/images/reviewss.webp"
           alt="Reviews Background"
           fill
           className="object-cover"
